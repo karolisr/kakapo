8669cc10c9cdd88f8fcc77995af020ce13599fe6
Karolis Ramanauskas <karolisr@gmail.com>
2019-08-14 08:24:00 -0500

    Rework of entrez and the related parsers modules.

--- kakapo/entrez.py
+++ kakapo/entrez.py
@@ -18,238 +18,228 @@ from __future__ import print_function
 from __future__ import with_statement
 
 import re
+import os
 
 from time import sleep
-from xmltodict import parse as parse_xml
+from xml.etree import ElementTree
 from math import ceil
 
 from kakapo.http_k import get
 from kakapo.http_k import post
 
-from kakapo.parsers import parse_efetch_sra_xml_text
+from kakapo.parsers import parse_efetch_sra_csv_text
 from kakapo.parsers import parse_gbseq_xml_text
 from kakapo.parsers import parse_esummary_xml_text
 
 ENTREZ_BASE_URL = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
-DELAY = 3
+DELAY = 1
 
 
-def esearch(db, term):
-    """
+def _check_for_api_key():
 
-    Wrap ESearch E-utility.
+    global_variables = globals()
 
-    :param db: Name of the Entrez database to search.
-    :type db: str
+    if 'ENTREZ_KEY' in global_variables:
+        ncbi_api_key = global_variables['ENTREZ_KEY']
+    elif 'ENTREZ_KEY' in os.environ:
+        ncbi_api_key = os.environ['ENTREZ_KEY']
+    else:
+        print('Warning: ENTREZ_KEY is not defined.')
+        ncbi_api_key = None
 
-    :param term: Search terms.
-    :type term: str
+    return ncbi_api_key
 
-    :returns: A dictionary with these keys: Database, Count, IdList, QueryKey,
-        WebEnv
-    :rtype: dict
-    """
+
+def esearch(term, db, api_key=None):  # noqa
+
+    if api_key is None:
+        api_key = _check_for_api_key()
+
     eutil = 'esearch.fcgi'
-
     url = ENTREZ_BASE_URL + eutil
 
-    params = {'db': db, 'term': term, 'rettype': 'count'}
-    response = get(url, params, 'xml')
-    total_count = int(parse_xml(response.text)['eSearchResult']['Count'])
+    params = {'db': db, 'term': term, 'idtype': 'acc', 'rettype': 'count',
+              'retmode': 'json', 'usehistory': 'y', 'api_key': api_key}
 
-    # Now download the uids
-    retmax = 5000
-    retstart = 0
-    query_key = ''
-    web_env = ''
+    response = get(url, params, 'json')
+    parsed = response.json()
+    esearch_result = parsed['esearchresult']
+    if 'ERROR' in esearch_result:
+        raise Exception(esearch_result['ERROR'])
+
+    record_count = int(esearch_result['count'])
+
+    ret_max = 5000
+    query_key = None
+    web_env = None
     id_set = set()
-    return_value = list()
 
-    for retstart in range(0, total_count, retmax):
+    for ret_start in range(0, record_count, ret_max):
 
-        params = {'db': db, 'query_key': query_key, 'WebEnv': web_env,
-                  'retstart': str(retstart), 'retmax': str(retmax),
-                  'usehistory': 'y', 'term': term}
+        if ret_start > 0:
+            sleep(DELAY)
 
-        response = get(url, params, 'xml')
+        params = {'db': db, 'term': term, 'idtype': 'acc',
+                  'retstart': ret_start, 'retmax': ret_max,
+                  'rettype': 'uilist', 'retmode': 'json', 'usehistory': 'y',
+                  'api_key': api_key}
 
-        data = parse_xml(response.text)['eSearchResult']
+        if query_key is not None:
+            params['query_key'] = query_key
 
-        if total_count == 1:
-            id_set.add(data['IdList']['Id'])
-        else:
-            for uid in data['IdList']['Id']:
-                id_set.add(uid)
+        if web_env is not None:
+            params['WebEnv'] = web_env
 
-        query_key = data['QueryKey']
-        web_env = data['WebEnv']
+        response = get(url, params, 'json')
+        parsed = response.json()
+        data = parsed['esearchresult']
 
-    id_list = list(id_set)
-    id_list.sort()
+        id_set = id_set | set(data['idlist'])
+        query_key = data['querykey']
+        web_env = data['webenv']
 
-    # if count >= 99999:
-    #     message = (
-    #         'There are more than 99,999 unique identifiers: {c}.')
-    #     message = message.format(c=count)
-    #     raise Error(message)
+    id_tuple = tuple(sorted(id_set))
 
-    return_value = {
-        'Database': db,
-        'Count': total_count,
-        'IdList': id_list,
-        'QueryKey': query_key,
-        'WebEnv': web_env}
+    return_dict = {
+        'db': db,
+        'record_count': record_count,
+        'ids': id_tuple,
+        'query_key': query_key,
+        'web_env': web_env}
 
-    sleep(DELAY)
-
-    return return_value
+    return return_dict
 
 
-def epost(db, id_list):
-    """
+def epost(ids, db, api_key=None):  # noqa
 
-    Wrap EPost E-utility.
+    if api_key is None:
+        api_key = _check_for_api_key()
 
-    :param db: Name of the Entrez database to search.
-    :type db: str
-
-    :param id_list: List of unique identifiers.
-    :type id_list: list
-
-    :returns: A dictionary with these keys: Database, Count, QueryKey, WebEnv
-    :rtype: dict
-
-    """
     eutil = 'epost.fcgi'
-
-    id_list_string = ','.join(id_list)
-
     url = ENTREZ_BASE_URL + eutil
-
-    data = {'db': db, 'id': id_list_string}
+    data = {'db': db, 'id': ','.join(ids), 'api_key': api_key}
 
     response = post(url, data, 'xml')
+    root = ElementTree.fromstring(response.text)
 
-    results = parse_xml(response.text)['ePostResult']
+    query_key = None
+    web_env = None
 
-    query_key = results['QueryKey']
-    web_env = results['WebEnv']
+    for child in root:
+        if child.tag == 'QueryKey':
+            query_key = child.text
+        if child.tag == 'WebEnv':
+            web_env = child.text
 
-    count = len(id_list)
+    record_count = len(ids)
 
-    return_value = {
-        'Database': db,
-        'Count': count,
-        'QueryKey': query_key,
-        'WebEnv': web_env}
+    return_dict = {
+        'db': db,
+        'record_count': record_count,
+        'query_key': query_key,
+        'web_env': web_env}
 
-    sleep(DELAY)
-
-    return return_value
+    return return_dict
 
 
-def efetch(data, parser, ret_type, retmode='xml'):
-    """
+def efetch_data(data, parser, ret_type=None, ret_mode='xml', api_key=None):  # noqa
 
-    Wrap EFetch E-utility.
+    if api_key is None:
+        api_key = _check_for_api_key()
 
-    :param data: A dictionary returned by :func:`esearch` or :func:`epost` with
-        these keys: Database, Count, QueryKey, WebEnv
-    :type data: dict
-
-    :param parser: A function that will be called to interpret downloaded data.
-        This function may be called several times as :func:`efetch` downloads
-        downloads data in batches.
-    :type parser: function
-
-    :param ret_type: Retrieval type. This parameter specifies the record view
-        returned, such as Abstract or MEDLINE from PubMed, or GenPept or FASTA
-        from protein.
-    :type ret_type: str
-
-    :returns: A list of one or more items which will be of the type produced by
-        the parser.
-    :rtype: list
-    """
     eutil = 'efetch.fcgi'
+    url = ENTREZ_BASE_URL + eutil
 
-    db = data['Database']
-    query_key = data['QueryKey']
-    web_env = data['WebEnv']
-    count = data['Count']
+    db = data['db']
+    record_count = data['record_count']
+    query_key = data['query_key']
+    web_env = data['web_env']
 
-    retmax = 500
-    retstart = 0
+    ret_max = 500
+    ret_start = 0
 
-    return_value = []
+    return_list = []
 
-    for retstart in range(0, count, retmax):
+    for ret_start in range(0, record_count, ret_max):
 
-        url = ENTREZ_BASE_URL + eutil
+        if ret_start > 0:
+            sleep(DELAY)
 
         params = {'db': db, 'query_key': query_key, 'WebEnv': web_env,
-                  'retstart': str(retstart), 'retmax': str(retmax),
-                  'rettype': ret_type, 'retmode': retmode}
+                  'retstart': ret_start, 'retmax': ret_max,
+                  'rettype': ret_type, 'retmode': ret_mode, 'usehistory': 'y',
+                  'api_key': api_key}
 
-        response = get(url, params, retmode)
-
+        response = get(url, params, ret_mode)
         parsed = parser(response.text)
 
         for item in parsed:
-            return_value.append(item)
+            return_list.append(item)
 
-    # ret_mode Retrieval mode. This parameter specifies the data format
-    #     of the records returned, such as plain text, HMTL or XML.
-
-    # See the link below for the possible values of ret_type and ret_mode:
-    #     http://www.ncbi.nlm.nih.gov/books/NBK25499/table/chapter4.T._valid_values_of__retmode_and/?report=objectonly  # noqa
-
-    sleep(DELAY)
-
-    return return_value
+    return return_list
 
 
-def esummary(data, parser):  # noqa
+def efetch(db, params, ret_type=None, ret_mode='xml', api_key=None):  # noqa
+
+    if api_key is None:
+        api_key = _check_for_api_key()
+
+    eutil = 'efetch.fcgi'
+    url = ENTREZ_BASE_URL + eutil
+
+    params_all = {'db': db, 'rettype': ret_type, 'retmode': ret_mode,
+                  'api_key': api_key}
+
+    params_all.update(params)
+
+    response = get(url, params_all, ret_mode)
+
+    return response.text
+
+
+def esummary(data, api_key=None):  # noqa
+
+    if api_key is None:
+        api_key = _check_for_api_key()
 
     eutil = 'esummary.fcgi'
 
-    db = data['Database']
-    query_key = data['QueryKey']
-    web_env = data['WebEnv']
-    count = data['Count']
+    db = data['db']
+    record_count = data['record_count']
+    query_key = data['query_key']
+    web_env = data['web_env']
 
-    retmax = 500
-    retstart = 0
+    ret_max = 500
 
-    return_value = []
+    return_list = []
 
-    for retstart in range(0, count, retmax):
+    for ret_start in range(0, record_count, ret_max):
+
+        if ret_start > 0:
+            sleep(DELAY)
 
         url = ENTREZ_BASE_URL + eutil
 
         params = {'db': db, 'query_key': query_key, 'WebEnv': web_env,
-                  'retstart': str(retstart), 'retmax': str(retmax),
-                  'rettype': 'docsum'}
+                  'retstart': ret_start, 'retmax': ret_max, 'retmode': 'json'}
 
-        response = get(url, params, 'xml')
+        response = get(url, params, 'json')
+        parsed = response.json()['result']
+        keys = parsed['uids']
 
-        parsed = parser(response.text)
+        for k in keys:
+            return_list.append(parsed[k])
 
-        for item in parsed:
-            return_value.append(item)
-
-    sleep(DELAY)
-
-    return return_value
+    return return_list
 
 
 def esearch_epost(term, db):  # noqa
-    if type(term) in [list, tuple]:
+    if type(term) in (list, tuple):
         term = ' OR '.join(term)
-    esearch_results = esearch(db, term)
-    id_list = esearch_results['IdList']
+    esearch_results = esearch(term, db)
+    id_list = esearch_results['ids']
     if len(id_list) > 0:
-        epost_results = epost(db, id_list)
+        epost_results = epost(id_list, db)
     else:
         epost_results = None
     return epost_results
@@ -265,29 +255,29 @@ def taxids_for_acc(accessions, db):  # noqa
     summ = summary(accessions, db)
     ret_dict = dict()
     for x in summ:
-        acc = x['AccessionVersion']
-        taxid = x['TaxId']
+        acc = x['accessionversion']
+        taxid = x['taxid']
         ret_dict[acc] = taxid
 
     return ret_dict
 
 def dnld_seqs(term, db):  # noqa
     epost_results = esearch_epost(term, db)
-    efetch_results = efetch(epost_results, parse_gbseq_xml_text, 'gb')
+    efetch_results = efetch_data(epost_results, parse_gbseq_xml_text, 'gb')
     return efetch_results
 
 
 def dnld_seqs_gb_format(term, db):  # noqa
     epost_results = esearch_epost(term, db)
-    efetch_results = efetch(epost_results, lambda x: x.split('\n\n')[:-1],
-                            'gb', 'plain_text')
+    efetch_results = efetch_data(epost_results, lambda x: x.split('\n\n')[:-1],
+                                 'gb', 'text')
     return efetch_results
 
 
 def dnld_cds_nt_fasta(term):  # noqa
     epost_results = esearch_epost(term, 'nuccore')
-    efetch_results = efetch(epost_results, lambda x: x.split('\n>'),
-                            'fasta_cds_na', 'plain_text')
+    efetch_results = efetch_data(epost_results, lambda x: x.split('\n>'),
+                                 'fasta_cds_na', 'text')
 
     ret_list = []
     for x in efetch_results:
@@ -336,14 +326,12 @@ def sra_run_info(acc_list):  # noqa
         term = ' OR '.join(temp_acc_list)
 
         epost_results = esearch_epost(term, 'sra')
+        if epost_results is None:
+            continue
 
-        efetch_results = efetch(epost_results, parse_efetch_sra_xml_text,
-                                'runinfo')
+        efetch_results = efetch_data(epost_results, parse_efetch_sra_csv_text,
+                                     'runinfo', 'csv')
 
-        temp = efetch_results[0]
-        if type(temp) is list:
-            ret_list = ret_list + temp
-        else:
-            ret_list = ret_list + efetch_results
+        ret_list = ret_list + efetch_results
 
     return ret_list
