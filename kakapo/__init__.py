1483dcd5e89c7bef69754aa30897bccbd9e78ae2
Karolis Ramanauskas <karolisr@gmail.com>
2020-02-01 17:24:43 -0600

    Update version number 1.0.0 -> 0.0.1.

--- kakapo/__init__.py
+++ kakapo/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __script_name__ = 'kakapo'
-__version__ = '1.0.0'
+__version__ = '0.0.1'
 __description__ = ('Extract and annotate protein family members from '
                    'transcriptomes.')
 __author__ = 'Karolis Ramanauskas'
