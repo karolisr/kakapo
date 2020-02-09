78c9baed5b15dd1f0272ef82a5f2cf4364d44345
Karolis Ramanauskas <karolisr@gmail.com>
2020-02-09 16:24:56 -0600

    Updates to the setup.py and related changes in other files.

--- kakapo/__init__.py
+++ kakapo/__init__.py
@@ -8,5 +8,6 @@ __author__ = 'Karolis Ramanauskas'
 __author_email__ = 'kraman2@uic.edu'
 __copyright__ = 'Copyright (c) 2019 Karolis Ramanauskas'
 __license__ = 'cc-by-sa-4.0'
+__url__ = 'https://github.com/karolisr/kakapo'
 
 __all__ = []
--- setup.py
+++ setup.py
@@ -1,50 +1,32 @@
 """
-Kakapo setup.
+Kakapo setup.py
 """
 
-# This is a workaround. pip, in some cases, will not find dependencies
-# on GitHub, so we install them directly.
-import sys
-import subprocess
-
-git_ntaxlocal = 'git+https://github.com/karolisr/ncbi-taxonomy-local'
-
-if '--user' in sys.argv:
-    subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade',
-                    '--user', git_ntaxlocal], check=False)
-else:
-    subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade',
-                    git_ntaxlocal], check=False)
-# end pip workaround.
-
-import setuptools  # noqa
+from setuptools import setup, find_packages
 
 from kakapo import __author__ as kakapo_author
 from kakapo import __author_email__ as kakapo_author_email
 from kakapo import __description__ as kakapo_description
 from kakapo import __script_name__ as kakapo_script_name
+from kakapo import __url__ as kakapo_url
 from kakapo import __version__ as kakapo_version
 
 with open('requirements.txt', 'r') as f:
     reqs = f.read()
 
-reqs = reqs.split('\n')[0:-2]
+reqs = reqs.splitlines()
 
-setuptools.setup(
-    name=kakapo_script_name,
-    version=kakapo_version,
-    author=kakapo_author,
-    author_email=kakapo_author_email,
-    description=kakapo_description,
-    long_description=kakapo_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/karolisr/kakapo',
-    packages=setuptools.find_packages(),
-    install_requires=reqs,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: cc-by-sa-4.0',
-        'Operating System :: OS Independent',
-    ],
-    entry_points={'console_scripts': ['kakapo=kakapo.__main__:main']}
-)
+setup(name=kakapo_script_name,
+      version=kakapo_version,
+      description=kakapo_description,
+      long_description=kakapo_description,
+      long_description_content_type='text/markdown',
+      author=kakapo_author,
+      author_email=kakapo_author_email,
+      maintainer=kakapo_author,
+      maintainer_email=kakapo_author_email,
+      url=kakapo_url,
+      packages=find_packages(),
+      install_requires=reqs,
+      entry_points={'console_scripts': ['kakapo=kakapo.__main__:main']}
+      )
