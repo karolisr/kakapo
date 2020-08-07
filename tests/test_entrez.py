c4807abec6dda2a2b5413d851d3a7586a62075ee
Karolis Ramanauskas <karolisr@gmail.com>
2020-08-07 12:42:56 -0500

    Removes old tests. Removes unnecessary files.

--- tests/test_entrez.py
+++
@@ -1,9 +0,0 @@
-from kakapo.tools.eutils import search
-
-
-def test_esearch():
-    esearch_results = search(db='protein', term='AAB87127.1')
-
-    assert esearch_results['db'] == 'protein'
-    assert isinstance(esearch_results['query_keys'], list)
-    assert isinstance(esearch_results['web_env'], str)
