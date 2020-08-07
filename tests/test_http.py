9fc449417ab7aafa3d00c7a7dea2c445535bc622
Karolis Ramanauskas <karolisr@gmail.com>
2020-08-07 13:00:27 -0500

    Adds basic tests for kakapo.utils.http module to help configure Travis CI.

---
+++ tests/test_http.py
@@ -0,0 +1,23 @@
+"""Tests for kakapo.utils.http module."""
+
+from kakapo.utils.http import get
+from requests.exceptions import RetryError
+
+
+class TestGet:
+    def test_get_retry_fail(self):
+        try:
+            r = get(url='http://httpbin.org/status/500',
+                    response_format='text')
+        except RetryError as e:
+            pass
+
+    def test_get_success(self):
+        r = get(url='http://httpbin.org/status/200',
+                response_format='text')
+        assert r.status_code == 200
+
+    def test_get_eventual_success(self):
+        r = get(url='http://httpbin.org/status/200,200,500',
+                response_format='text')
+        assert r.status_code == 200
