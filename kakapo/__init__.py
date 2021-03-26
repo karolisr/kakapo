f3e70364cb7ff35b5b3a69535031fa6d13222bf5
Karolis Ramanauskas <karolisr@gmail.com>
2021-03-26 15:19:09 -0500

    v0.7.6 -> v0.7.7.

--- kakapo/__init__.py
+++ kakapo/__init__.py
@@ -6,7 +6,7 @@ date_time = datetime.now()
 y = str(date_time.year)
 
 __script_name__ = 'kakapo'
-__version__ = '0.7.6'
+__version__ = '0.7.7'
 __description__ = ('Extract and annotate protein family members from '
                    'transcriptomes.')
 __author__ = 'Karolis Ramanauskas'
