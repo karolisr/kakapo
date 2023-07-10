db56500d56d29be0051ad371a0ab33908b41797f
Karolis Ramanauskas <karolisr@gmail.com>
2023-07-09 20:00:21 -0500

    Updates "run_cluster_fast" function with more options.

--- kakapo/tools/vsearch.py
+++ kakapo/tools/vsearch.py
@@ -3,15 +3,27 @@
 from kakapo.utils.subp import run
 
 
-def run_cluster_fast(vsearch, ident, in_file, out_file):
+def run_cluster_fast(vsearch, ident, in_file, out_file_centroids,
+                     out_file_prefix_clusters=None,
+                     clusterout_id=False,
+                     iddef=None):
 
     cmd = [vsearch,
            '--cluster_fast', in_file,
-           '--centroids', out_file,
+           '--centroids', out_file_centroids,
            '--fasta_width', '0',
            '--id', str(ident)]
 
-    run(cmd, do_not_raise=True)
+    if clusterout_id is True:
+        cmd.append('--clusterout_id')
+
+    if iddef is not None:
+        cmd += ['--iddef', str(iddef)]
+
+    if out_file_prefix_clusters is not None:
+        cmd += ['--clusters', out_file_prefix_clusters]
+
+    run(cmd, do_not_raise=False)
 
 
 def run_vsearch(vsearch, ident, q_file, db_file, out_file, minlen):
